5b687663eacf7336afb35d2f120bee51c7f968a6
Dmiterev <113352857+Dmiterev@users.noreply.github.com>
2023-06-02 14:44:03 +0400

    enumeration

---
+++ enumeration.py
@@ -0,0 +1,33 @@
+import hashlib
+import multiprocessing as mp
+from tqdm import tqdm
+
+
+def enumerate_number(hash: str, bin: str, last_numbers: str, core_number: int = mp.cpu_count()):
+    """
+    Подбор номера карты.
+    :param hash: Хэш карты.
+    :param bin: БИН номер карты.
+    :param last_numbers: Последние 4 цифры карты.
+    :param core_number: Количество ядер.
+    """
+    args = []
+    for i in range(1000000):
+        args.append((hash, f"{bin}{i:06d}{last_numbers}"))
+    with mp.Pool(processes=core_number) as p:
+        for result in p.starmap(check_number, tqdm(args, desc="Процесс нахождения номера карты: ", ncols=120)):
+            if result:
+                p.terminate()
+                return result
+    return None
+
+
+def check_number(hash: str, card_number: str):
+    """
+    Проверка соответствия номера с хэшем.
+    :param hash: Хэш карты.
+    :param card_number: Номер карты.
+    """
+    if hash == hashlib.sha3_384(card_number.encode()).hexdigest():
+        return card_number
+    return False
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-import argparse as args
+import argparse
 import logging
 import json
+from enumeration import enumerate_number
 
 logger = logging.getLogger()
 logger.setLevel('INFO')
@@ -21,11 +22,40 @@ def read_settings(file: str) -> dict:
     return json_data
 
 
+def write_in_file(number_file: str, number: str):
+    """
+    Запись номера в файл.
+    :param number_file: Путь к файлу.
+    :param number: Номер карты.
+    """
+    try:
+        with open(number_file, 'w') as f:
+            f.write(number)
+        logging.info(f'Номер карты записан в {number_file}!')
+    except OSError as err:
+        logging.warning(f'{err} Ошибка при записи номера карты в {number_file}!')
+
+
 if __name__ == '__main__':
-    parser = args.ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument('-set', '--settings', type=str,
-                        help='Использовать собственный файл c настройками (Указать путь к файлу)')
+                        help='Использовать собственный файл c настройками. Указать путь к файлу')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-enu', '--enumeration', type=int,
+                       help='Определение номера карты с помощью хеша. Указать количество процессов')
+    args = parser.parse_args()
     if args.settings:
         settings = read_settings(args.settings)
     else:
         settings = read_settings(os.path.join("data", "settings.json"))
+    if settings:
+        if args.enumeration:
+            hash = settings["hash"]
+            bin = settings["bin"]
+            last_numbers = settings["last_numbers"]
+            card_number = enumerate_number(hash, bin, last_numbers, args.enumeration)
+            if card_number:
+                logging.info(f"Полученный номер карты: {card_number}")
+                write_in_file(settings["card_number"], card_number)
+            else:
+                logging.info("Номер карты не найден!")
